796322b02e919069f3efd843c78b47731c56c561
s64309010015 <s64309010015@ptnicec.ac.th>
2022-10-21 11:28:48 +0700

    ส่งไฟล์ test ตรั้งที่1

--- test.php
+++ test.php
@@ -1,6 +1,22 @@
 <?php
-
-    echo "Fatimoh Mayutae";
-
-
+    echo "Fatimoh Mayutae<br>";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TEST</title>
+</head>
+<body>
+     Information Technology 
+    <br>
+    <?php
+        echo "Pattani";
+    ?>
+</body>
+</html>
+<?php
+   echo "<br>Thailand";
 ?>
